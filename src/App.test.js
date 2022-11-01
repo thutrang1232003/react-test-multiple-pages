3e33f8946bdc1415e3b15e7f2bf0c2e870007818
Tran Thi Thu Trang <thutrang1232003@gmail.com>
2022-11-01 13:15:39 +0200

    changes content in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/How to play/i);
+  const linkElement = screen.getByText(/This is a place/i);
   expect(linkElement).toBeInTheDocument();
 });
